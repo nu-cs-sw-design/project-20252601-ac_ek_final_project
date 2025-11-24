c05cf939a2d3a78f79a4da21382aa440894c78e6
Aryaman Chawla <aryamanchawla2025@u.northwestern.edu>
2025-11-24 16:35:23 -0600

    change attack cards to use interface + fix missing functionality & bad code practices

--- src/main/java/domain/model/cards/attack/AttackCard.java
+++ src/main/java/domain/model/cards/attack/AttackCard.java
@@ -1,40 +1,45 @@
 package domain.model.cards.attack;
 
-import domain.model.Game;
+import domain.model.GameContext;
 import domain.model.Player;
 import domain.model.cards.Card;
-import ui.UI;
+import domain.model.cards.CardEffect;
 
 public class AttackCard extends Card {
     private static final int[] COUNTS = {2, 3, 5};
     private static final int NUM_TURNS_TO_ADD = 2;
     private static final int SET_CURRENT_PLAYER_TURNS = 0;
 
+    public AttackCard() {
+        super(new AttackCardEffect());
+    }
+
     @Override
     public String getName() {
         return "Attack";
     }
 
-    @Override
-    public void playCard(Game game, UI ui) {
-        ui.displayMessage("attackCard");
-
-        Player currentPlayer = game.getCurrentPlayer();
-        int nextPlayerTurns;
-        if (currentPlayer.getNumberOfTurns() == 1) {
-            nextPlayerTurns = NUM_TURNS_TO_ADD;
-        } else {
-            nextPlayerTurns = currentPlayer.getNumberOfTurns() + NUM_TURNS_TO_ADD;
-        }
-        game.setCurrentPlayerTurns(SET_CURRENT_PLAYER_TURNS);
-
-        int cardIndex = currentPlayer.hasCard(this.getName());
-        game.removeCurrentPlayerCard(cardIndex);
-
-        game.setNextPlayerTurns(nextPlayerTurns);
-    }
-
     public static int[] getCounts() {
         return COUNTS.clone();
+    }
+
+    private static class AttackCardEffect implements CardEffect {
+        @Override
+        public void execute(GameContext context) {
+            context.displayMessage("attackCard");
+
+            Player currentPlayer = context.getCurrentPlayer();
+            int nextPlayerTurns;
+            if (currentPlayer.getNumberOfTurns() == 1) {
+                nextPlayerTurns = NUM_TURNS_TO_ADD;
+            } else {
+                nextPlayerTurns = currentPlayer.getNumberOfTurns() + NUM_TURNS_TO_ADD;
+            }
+            context.setCurrentPlayerTurns(SET_CURRENT_PLAYER_TURNS);
+            context.setNextPlayerTurns(nextPlayerTurns);
+            
+            int cardIndex = currentPlayer.hasCard("Attack");
+            context.removeCurrentPlayerCard(cardIndex);
+        }
     }
 }
--- src/main/java/domain/model/cards/attack/TargetedAttackCard.java
+++ src/main/java/domain/model/cards/attack/TargetedAttackCard.java
@@ -1,49 +1,58 @@
 package domain.model.cards.attack;
 
-import domain.model.Game;
+import domain.model.GameContext;
 import domain.model.Player;
 import domain.model.cards.Card;
-import ui.UI;
+import domain.model.cards.CardEffect;
 
 public class TargetedAttackCard extends Card{
     private static final int[] COUNTS = {5, 6, 8};
+    private static final int SET_CURRENT_PLAYER_TURNS = 0;
+
+    public TargetedAttackCard() {
+        super(new TargetedAttackCardEffect());
+    }
 
     @Override
     public String getName() {
         return "Targeted Attack";
     }
 
-    @Override
-    public void playCard(Game game, UI ui) {
-        ui.displayMessage("targetedAttackCard");
-
-        Player currentPlayer = game.getCurrentPlayer();
-        int targetPlayerId = ui.promptPlayer("targetPlayerId");
-        if (targetPlayerId <= 0 || targetPlayerId > game.getPlayers().size()) {
-            throw new UnsupportedOperationException("invalidPlayerID");
-        }
-
-        if (currentPlayer.getId() == targetPlayerId) {
-            throw new UnsupportedOperationException("chosenSelfError");
-        }
-
-        int currentPlayerTurns = currentPlayer.getNumberOfTurns();
-        int nextPlayerTurns;
-
-        if (currentPlayerTurns == 1) {
-            nextPlayerTurns = 2;
-        } else {
-            nextPlayerTurns = currentPlayerTurns + 2;
-        }
-
-        game.setPlayerTurns(targetPlayerId, nextPlayerTurns);
-        game.setCurrentPlayerTurns(0);
-
-        int cardIndex = currentPlayer.hasCard(this.getName());
-        game.removeCurrentPlayerCard(cardIndex);
-    }
-
     public static int[] getCounts() {
         return COUNTS.clone();
+    }
+
+    private static class TargetedAttackCardEffect implements CardEffect {
+        @Override
+        public void execute(GameContext context) {
+            context.displayMessage("targetedAttackCard");
+
+            Player currentPlayer = context.getCurrentPlayer();
+            int targetPlayerId = context.promptPlayer("targetPlayerId");
+            if (targetPlayerId <= 0 || targetPlayerId > context.getPlayers().size()) {
+                throw new UnsupportedOperationException("invalidPlayerID");
+            }
+            if (currentPlayer.getId() == targetPlayerId) {
+                throw new UnsupportedOperationException("chosenSelfError");
+            }
+
+            int currentPlayerTurns = currentPlayer.getNumberOfTurns();
+            int nextPlayerTurns;
+            if (currentPlayerTurns == 1) {
+                nextPlayerTurns = 2;
+            } else {
+                nextPlayerTurns = currentPlayerTurns + 2;
+            }
+
+            context.setCurrentPlayerTurns(SET_CURRENT_PLAYER_TURNS);
+
+            int cardIndex = currentPlayer.hasCard("Targeted Attack");
+            context.removeCurrentPlayerCard(cardIndex);
+
+            Player targetPlayer = context.getPlayer(targetPlayerId);
+            targetPlayer.setNumberOfTurns(nextPlayerTurns);
+            context.setPlayer(targetPlayer);
+            context.setNextPlayerTargetPlayer(targetPlayer);
+        }
     }
 }
